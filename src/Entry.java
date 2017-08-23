1948127c488a2b0e39f207553f8b9866dc2489bb
nidhin mahesh <nidhinmahesh1@gmail.com>
2017-08-24 00:28:02 +0530

    buffered reader to string
    trying to get contents of file into string

--- src/Entry.java
+++ src/Entry.java
@@ -4,20 +4,24 @@ import java.util.Scanner;
 public class Entry{
     Status stEnt = new Status();
     Scanner s = new Scanner(System.in);
-    // public Entry(){
-
-    // }
     public int lines(){
-    try {
-        int line = 0;
-        BufferedReader reader = new BufferedReader(new FileReader("features.txt"));
-        while((reader.readLine()) != null){
-            line++;
-        }
-        reader.close();
+        try {
+            int line = 0;
+            
+            BufferedReader reader = new BufferedReader(new FileReader("features.txt"));
+            while((reader.readLine()) != null){
+                line++;
+            }
+            //writes features into array
+            int i=0;
+            String[] feats = new String[line];
+            while((reader.readLine()) != null){
+                // feats[i] = org.apache.commons.io.IOUtils.toString(reader);
+                i++;
+            }
+            reader.close();
         return line;
-        
-        } 
+        }
         catch (Exception e) {
             System.out.println("Oops! Cannot fetch features..");
             return 0;
@@ -27,11 +31,13 @@ public class Entry{
         System.out.println(ln+" queries needed");
         
         try{
+            //create student file name
             System.out.print("Enter student roll number: ");
             String ROLL_NUMBER = s.next();
             String name = ROLL_NUMBER + ".txt";
             FileWriter out = null;
             out = new FileWriter(name);
+            //write file student details
             for(int num = 1; num<=ln; num++){
 
             }
