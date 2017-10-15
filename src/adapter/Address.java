16ac1429424eada799f7bc6483007731f0377336
nidhin mahesh <nidhinmahesh1@gmail.com>
2017-10-15 14:49:56 +0530

    added docs

---
+++ src/adapter/Address.java
@@ -0,0 +1,13 @@
+package adapter;
+public class Address{
+    String qa = "Enter student address: ";
+    public String GET(){
+        return qa;
+    }
+    public static void FIND(){
+
+    }
+    public static void POST(){
+
+    }
+}
