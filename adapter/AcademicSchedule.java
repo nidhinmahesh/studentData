6c60b6a2c2d4a94db997ca1ea694fce50411ce0d
nidhin mahesh <nidhinmahesh1@gmail.com>
2017-11-07 00:58:12 +0530

    add schedule

---
+++ adapter/AcademicSchedule.java
@@ -0,0 +1,27 @@
+public class AcademicSchedule{
+
+    public void newSchedule(){
+
+        ArrayList<String> ar = new ArrayList<String>();
+        //a file for teacher message. Will be read up when the array is not empty
+        try{
+            String line;
+            BufferedReader reader = new BufferedReader(new FileReader("Schedule.txt"));
+            while((line = reader.readLine())!= null)
+            {
+                ar.add(line);
+            }       
+
+            System.out.println("Add your schedule. press ENTER when done : ");  
+            String currentMsg = s.nextLine();
+            ar.add(currentMsg);
+            System.out.println("Done.");
+        }        
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+
+    }
+}
