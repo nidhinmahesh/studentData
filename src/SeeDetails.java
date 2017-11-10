34e2cc1991e1ecc9c81f72701a3f6b5b94b873da
nidhin mahesh <nidhinmahesh1@gmail.com>
2017-11-10 22:37:35 +0530

    search from file

--- src/SeeDetails.java
+++ src/SeeDetails.java
@@ -2,6 +2,24 @@ public class SeeDetails{
 
     public void getDetails()
     {
-        
+        try{
+
+            File file = new File("bio.txt");
+            final Scanner scan = new Scanner(file);
+
+             while(scan.hasNextLine()){
+                final String lineFromFile = scan.nextLine();
+                 if(lineFromFile.contains(name)){
+
+
+
+                    System.out.println("I found " +name+ " in file " +file.getName());
+
+
+                    
+                 }
+              }
+            
+        }
     }
 }
